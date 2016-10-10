0e1d47344bc47f4f63a347364e2a038b5249ad88
yanlongzhu <yanlongzhu2010@gmail.com>
2016-10-10 16:28:41 +0800

    add

---
+++ src/main/java/com/kennyzhu/wx/spring/WxApplication.java
@@ -0,0 +1,16 @@
+package com.kennyzhu.wx.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yanlongzhu on 2016/10/10.
+ */
+@SpringBootApplication
+public class WxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxApplication.class, args);
+    }
+
+}
